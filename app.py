241c818e7b206f6a8a8a9fa1c8c5b5b8ef7cc0c0
Lorena Perez <lorenaperez@ufm.edu>
2022-09-27 19:46:15 -0600

    app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from sre_parse import State
 import list
 import stack
 import numpy as np
@@ -6,27 +7,75 @@ from tabulate import tabulate
 gram1 = ["x", "$"]
 gram2 = ["n"]
 gram3 = ["x", "+", "(", "x", ")"]
-enc = ["/", "(", ")", "x", "n", "+", "$"]
+enc = ["/", "x", "+", "n", "(", ")", "$"]
 s0 = ["s0", "g s1", "E", "g s2", "E", "E", "E"]
 s1 = ["s1", "E", "s s3", "E", "E", "E", "ok"]
 s2 = ["s2", "r2", "r2", "r2", "r2", "r2", "r2"]
 s3 = ["s3", "E", "E", "E", "g s4", "E", "E"]
 s4 = ["s4", "g s5", "E", "g s2", "s s4", "E", "E"]
-s5 = ["s5", "E", "g s3", "E", "E", "s s6", "E"]
+s5 = ["s5", "E", "s s3", "E", "E", "s s6", "E"]
 s6 = ["s6", "r3", "r3", "r3","r3","r3","r3"]
 
-m_data = np.full((8,7), "     ")
+m_gram = np.full((8,7), "     ")
 for i in range(len(enc)):
-    m_data[0,i] = enc[i]
-    m_data[1,i] = s0[i]
-    m_data[2,i] = s1[i]
-    m_data[3,i] = s2[i]
-    m_data[4,i] = s3[i]
-    m_data[5,i] = s4[i]
-    m_data[6,i] = s5[i]
-    m_data[7,i] = s6[i]
+    m_gram[0,i] = enc[i]
+    m_gram[1,i] = s0[i]
+    m_gram[2,i] = s1[i]
+    m_gram[3,i] = s2[i]
+    m_gram[4,i] = s3[i]
+    m_gram[5,i] = s4[i]
+    m_gram[6,i] = s5[i]
+    m_gram[7,i] = s6[i]
+
+# tabla de parseo
+#print(tabulate(m_gram, tablefmt="fancy_grid"))
+
+#stacks
+estado = stack.Stack(1)
+accion = stack.Stack(2)
+cad = list.List()
+#------------------------------
+
+def shift(rut, val):
     
-print(tabulate(m_data, tablefmt="fancy_grid"))
+
+#-------------- lectura de datos
+
+cl = 0
+cc = 0
+mcc = 0
+with open("data.txt","r") as archivo:
+    for linea in archivo:
+        cl =  cl + 1
+        cc = 0
+        for caracter in linea:
+            if ord(caracter) == 10:
+                continue
+            else:
+                cc = cc + 1
+        if mcc < cc:
+            mcc = cc 
+
+# matrices de datos
+m_data = np.full((cl,mcc), " ")
+m_resp = np.zeros((7,mcc))
+i = 0
+j = 0
+
+
+# lectura desde un txt
+with open("data.txt","r") as archivo:
+    j = 0
+    for linea in archivo:
+        i = 0
+        for caracter in linea:
+            if ord(caracter) == 10:
+                j = j + 1
+            else:
+                m_data[j,i] = caracter
+                i = i + 1
+print(m_data)
+print(m_resp)
 
 a = list.List()
 a.add(2)
